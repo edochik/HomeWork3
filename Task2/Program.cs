1b5c6bc08178e203d0b6248b1c4ec43847ae9778
edochik <edoch@yandex.ru>
2022-04-24 02:40:01 +0300

    add task3

---
+++ Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿double length(double xa, double xb, double ya, double yb)
+{
+    double lengthX = xa - xb;
+    double lengthY = ya - yb;
+    double lengthXY = Math.Sqrt((lengthX * lengthX) + (lengthY * lengthY));
+    return lengthXY;
+}
+
+double xa = 1;
+double xb = 2;
+double ya = 3;
+double yb = 5;
+Console.WriteLine("length " + Math.Round(length(xa, xb, ya, yb), 2));
