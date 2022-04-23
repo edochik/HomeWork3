836a4f13cbef52a01634ffa5fafcbef513290597
edochik <edoch@yandex.ru>
2022-04-24 02:39:28 +0300

    add task1

---
+++ Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿int axisXY(int x, int y)
+{
+    if (x > 0 && y > 0) Console.WriteLine(1);
+    if (x > 0 && y < 0) Console.WriteLine(2);
+    if (x < 0 && y < 0) Console.WriteLine(3);
+    if (x < 0 && y > 0) Console.WriteLine(4);
+    return x ^ y;
+}
+
+int x = -1;
+int y = 1;
+axisXY(x, y);
