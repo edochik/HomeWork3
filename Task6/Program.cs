5e9bdd16ae45a7b5047e73c969e7bc68792d8d22
edochik <edoch@yandex.ru>
2022-04-24 14:42:06 +0300

    add task6

---
+++ Task6/Program.cs
@@ -0,0 +1,13 @@
+﻿double threed(double xa, double xb, double ya, double yb, double za, double zb)
+{
+    double threedX = xa - xb;
+    double threedY = ya - yb;
+    double threedZ = za - zb;
+    double threeXYZ = Math.Sqrt((threedX * threedX) + (threedY * threedY) + (threedZ * threedZ));
+    return threeXYZ;
+}
+
+double xa = 7, xb = 1,
+       ya = -5, yb = -1,
+        za = 0, zb = 9;
+Console.WriteLine("lenght " + Math.Round(threed(xa, xb, ya, yb, za, zb), 2));
