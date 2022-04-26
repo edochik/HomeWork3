9fc45f6145691220bae68fe18a3b878def0357b9
edochik <edoch@yandex.ru>
2022-04-26 18:01:08 +0300

    add files png

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,13 +1,17 @@
-﻿double length(double xa, double xb, double ya, double yb)
+﻿//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние 
+//между ними в 2D пространстве.
+//A (3,6); B (2,1) -> 5,09  
+//A (7,-5); B (1,-1) -> 7,21
+
+double findDistance(double xa, double xb, double ya, double yb)
 {
-    double lengthX = xa - xb;
-    double lengthY = ya - yb;
-    double lengthXY = Math.Sqrt((lengthX * lengthX) + (lengthY * lengthY));
-    return lengthXY;
+    double distancX = xa - xb;
+    double distancY = ya - yb;
+    double distanceXY = Math.Sqrt((distancX * distancX) + (distancY * distancY));
+    return distanceXY;
 }
 
-double xa = 1;
-double xb = 2;
-double ya = 3;
-double yb = 5;
-Console.WriteLine("length " + Math.Round(length(xa, xb, ya, yb), 2));
+double xa = 7, xb = 1,
+       ya = -5, yb = -1;
+
+Console.WriteLine(Math.Round(findDistance(xa, xb, ya, yb),3));
