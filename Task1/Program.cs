c2b9b878d7cd7b241c368d46d53ad47b40e7821d
edochik <edoch@yandex.ru>
2022-04-26 18:01:32 +0300

    add files Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,4 +1,8 @@
-﻿int axisXY(int x, int y)
+﻿//Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и 
+//выдаёт номер четверти плоскости, в которой находится эта точка.
+
+
+int axisXY(int x, int y)
 {
     if (x > 0 && y > 0) Console.WriteLine(1);
     if (x > 0 && y < 0) Console.WriteLine(2);
@@ -7,6 +11,6 @@
     return x ^ y;
 }
 
-int x = -1;
-int y = 1;
+int x = -3;
+int y = 34;
 axisXY(x, y);
